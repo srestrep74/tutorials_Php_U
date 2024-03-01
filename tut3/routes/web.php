b90ddeab985684624e77ad70cdd28a8acb8eb52a
srestrep74 <srestrep74@eafit.edu.co>
2024-03-01 14:53:38 -0500

    Finished Tut3

---
+++ tut3/routes/web.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+
+Route::get('/about', function () {
+
+    $data1 = 'About us - Online Store';
+
+    $data2 = 'About us';
+
+    $description = 'This is an about page ...';
+
+    $author = 'Developed by: Your Name';
+
+    return view('home.about')->with('title', $data1)
+
+        ->with('subtitle', $data2)
+
+        ->with('description', $description)
+
+        ->with('author', $author);
+})->name('home.about');
+
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+Route::get('/cart', 'App\Http\Controllers\CartController@index')->name('cart.index');
+
+Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name('cart.add');
+
+Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name('cart.removeAll');
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name('image.index');
+
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name('image.save');
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name('imagenotdi.index');
+
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name('imagenotdi.save');
+
+Auth::routes();
